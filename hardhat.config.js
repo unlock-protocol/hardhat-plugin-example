ddcef2d84ccd8a3302114b5d55bc5ebf12456a20
Clement Renaud <clement@clementrenaud.com>
2022-02-14 11:51:58 +0100

    show duration and price on cli lock info

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,6 @@
+const { constants } = require("ethers");
+const humanizeDuration = require('humanize-duration')
+
 require("@unlock-protocol/hardhat-plugin");
 
 task('lock:info', "Prints some info about a lock")
@@ -13,13 +16,16 @@ task('lock:info', "Prints some info about a lock")
 
     // fetch potential ERC20 token address
     const tokenAddress = await lock.tokenAddress()
+    const maxNumberOfKeys = ethers.BigNumber.from(await lock.maxNumberOfKeys())
+    const expirationDuration = ethers.BigNumber.from(await lock.expirationDuration())
     
     console.log(
       `LOCK \n`,
       ` - name: '${await lock.name()}' \n`,
       ` - address: ${lock.address} \n`,
-      ` - keys: ${await lock.totalSupply()} / ${await lock.maxNumberOfKeys()} \n`,
-      ` - owners: ${await lock.numberOfOwners()} \n`,
+      ` - price: ${ethers.utils.formatEther(ethers.BigNumber.from(await lock.keyPrice())) } ETH \n`,
+      ` - duration: ${expirationDuration.eq(constants.MaxUint256) ? 'permanent' : humanizeDuration(expirationDuration.toNumber()*1000)} \n`,
+      ` - keys: ${await lock.totalSupply()} / ${maxNumberOfKeys.eq(constants.MaxUint256) ? '∞' : maxNumberOfKeys.toString() } \n`,
       ` - currency: ${tokenAddress === ethers.constants.AddressZero ? 'ETH' : tokenAddress } \n`,
       ` - balance: ${ethers.utils.formatUnits(await ethers.provider.getBalance(lock.address), 18)} \n`,
       ` - symbol: ${await lock.symbol()} \n`,
